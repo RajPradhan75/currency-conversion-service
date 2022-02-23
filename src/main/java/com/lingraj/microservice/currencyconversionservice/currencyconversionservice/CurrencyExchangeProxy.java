1dbe622e37305600ddcc99a66d303eac4050e99b
Raj8655 <42677693+Raj8655@users.noreply.github.com>
2022-02-23 16:28:04 +0530

    First Commit

---
+++ src/main/java/com/lingraj/microservice/currencyconversionservice/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,12 @@
+package com.lingraj.microservice.currencyconversionservice.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+
+}
